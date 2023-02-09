a970e4de1691c4aaa4a8096a53f98d3c89e1ddbd
[1] <[1.asus.rfrf]>
2023-02-09 12:31:04 +0300

    Add files

---
+++ Ex_01.py
@@ -0,0 +1,19 @@
+﻿from progress.bar import Bar
+import time
+import random
+
+title = "Процесс    |Уровень загрузки                | Шаг загрузки и время выполнения"
+print('-'*len(title))
+print(title)
+print('-'*len(title))
+bar = Bar('Processing', max=20)
+for i in range(20):
+    a = random.random()
+    time.sleep(a)
+    bar.next()
+    print(f'   {a}')
+bar.finish()
+print('-'*len(title))
+print("Готово")
+print('-'*len(title))
+print()
